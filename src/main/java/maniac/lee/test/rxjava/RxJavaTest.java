c27cb76285ae00723855feb9d27c22e8a678f383
lipeng <dianxiangan@aliyun.com>
2016-03-11 15:56:39 +0800

    rx

--- src/main/java/maniac/lee/test/rxjava/RxJavaTest.java
+++ src/main/java/maniac/lee/test/rxjava/RxJavaTest.java
@@ -1,5 +1,6 @@
 package maniac.lee.test.rxjava;
 
+import com.google.common.collect.Lists;
 import org.junit.Test;
 import rx.Observable;
 
@@ -12,6 +13,17 @@ public class RxJavaTest {
     public void simple() {
         Observable.just("Hello, world!")
                 .subscribe(s -> System.out.println(s));
+        hello("fuck", "you");
     }
 
+    public static void hello(String... names) {
+        Observable.from(names).subscribe(s -> {
+            System.out.println("Hello " + s + "!");
+        });
+        Observable.just("start").doOnNext(s -> System.out.println("next->" + s))
+                .doOnNext(s1 -> System.out.println("next next->" + s1))
+                .map(s3 -> Lists.newArrayList(s3))
+                .subscribe(s2 -> System.out.println("end->" + s2))
+        ;
+    }
 }
