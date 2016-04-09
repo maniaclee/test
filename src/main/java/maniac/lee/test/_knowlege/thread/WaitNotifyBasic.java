251a02aa4397679f2fd55b51dd57c79092f4bd0d
peng <dianxiangan@aliyun.com>
2016-04-09 11:36:05 +0800

    thread

---
+++ src/main/java/maniac/lee/test/_knowlege/thread/WaitNotifyBasic.java
@@ -0,0 +1,47 @@
+package maniac.lee.test._knowlege.thread;
+
+/***
+ * 1. wait()是阻塞持有该对象锁的当前线程
+ * 2. wait()必须放在synchronize块里
+ */
+public class WaitNotifyBasic {
+    public static Object object = new Object();
+    public static void main(String[] args) {
+        Thread1 thread1 = new Thread1();
+        Thread2 thread2 = new Thread2();
+         
+        thread1.start();
+         
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+         
+        thread2.start();
+    }
+     
+    static class Thread1 extends Thread{
+        @Override
+        public void run() {
+            synchronized (object) {
+                try {
+                    object.wait();
+                } catch (InterruptedException e) {
+                }
+                System.out.println("线程"+Thread.currentThread().getName()+"获取到了锁");
+            }
+        }
+    }
+     
+    static class Thread2 extends Thread{
+        @Override
+        public void run() {
+            synchronized (object) {
+                object.notify();
+                System.out.println("线程"+Thread.currentThread().getName()+"调用了object.notify()");
+            }
+            System.out.println("线程"+Thread.currentThread().getName()+"释放了锁");
+        }
+    }
+}
