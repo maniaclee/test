bf5300952a562078e7f7d93acee3b3df87cf885b
lipeng <dianxiangan@aliyun.com>
2016-08-04 19:47:40 +0800

    fx

---
+++ src/main/java/maniac/lee/test/userrpc/Consumer.java
@@ -0,0 +1,45 @@
+package maniac.lee.test.userrpc;
+
+import com.lvbby.user.api.dto.UserDTO;
+import com.lvbby.user.api.service.UserService;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.Date;
+
+@SpringBootApplication
+@RunWith(SpringJUnit4ClassRunner.class)
+@EnableAspectJAutoProxy
+@ImportResource("userrpc/client.xml")
+@ComponentScan("maniac.lee.test.userrpc")
+public class Consumer {
+
+    @Autowired
+    UserService userService;
+
+    public void esdf(){
+        
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"userrpc/client.xml"});
+        context.start();
+
+        UserService userService = (UserService) context.getBean("userService");// 获取远程服务代理
+//        UserService userService = context.getBean(UserService.class);// 获取远程服务代理
+        UserDTO entity = new UserDTO();
+        entity.setName("sdsfsdf");
+        entity.setAddTime(new Date());
+        long re = userService.add(entity);
+        System.out.println("add : ===> " + re);
+
+    }
+
+}
