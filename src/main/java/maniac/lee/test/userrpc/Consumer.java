4dffa8efff53e42b813bf9a35ef810f04b2a4c87
lipeng <dianxiangan@aliyun.com>
2016-08-05 14:22:26 +0800

    dubbo using zookeeper:// protocal!

--- src/main/java/maniac/lee/test/userrpc/Consumer.java
+++ src/main/java/maniac/lee/test/userrpc/Consumer.java
@@ -2,13 +2,18 @@ package maniac.lee.test.userrpc;
 
 import com.lvbby.user.api.dto.UserDTO;
 import com.lvbby.user.api.service.UserService;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.Date;
@@ -18,13 +23,20 @@ import java.util.Date;
 @EnableAspectJAutoProxy
 @ImportResource("userrpc/client.xml")
 @ComponentScan("maniac.lee.test.userrpc")
+@ContextConfiguration(classes = {Consumer.class})
+@EnableAutoConfiguration(exclude = {DataSourceTransactionManagerAutoConfiguration.class, DataSourceAutoConfiguration.class})
 public class Consumer {
 
     @Autowired
     UserService userService;
 
+    @Test
     public void esdf(){
-        
+        UserDTO entity = new UserDTO();
+        entity.setName("sdsfsdf");
+        entity.setAddTime(new Date());
+        long re = userService.add(entity);
+        System.out.println("add : ===> " + re);
     }
 
 
