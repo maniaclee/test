10ce545729d63799f66fead2ac10dbcd81a8b192
lipeng <dianxiangan@aliyun.com>
2016-10-19 16:26:48 +0800

    refactor

--- src/main/java/com/lvbby/bridge/spring/test/BridgeSpringTest.java
+++ src/main/java/com/lvbby/bridge/spring/test/BridgeSpringTest.java
@@ -3,7 +3,7 @@ package com.lvbby.bridge.spring.test;
 import com.lvbby.bridge.exception.BridgeException;
 import com.lvbby.bridge.gateway.Bridge;
 import com.lvbby.bridge.gateway.Context;
-import com.lvbby.bridge.gateway.Params;
+import com.lvbby.bridge.api.Params;
 import com.lvbby.bridge.spring.test.spring.Config;
 import org.junit.Test;
 import org.junit.runner.RunWith;
